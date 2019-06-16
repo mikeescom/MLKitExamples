f2fbfcf93a6a69563b7b3dd353e37f5e9ed1d568
msmikeescom <mM02071980>
2019-06-15 20:22:18 -0700

    Image labeling (Firebase cloud vision implementation).

--- app/src/main/java/com/mikeescom/mlkitexamples/ui/ImageLabelingActivity.java
+++ app/src/main/java/com/mikeescom/mlkitexamples/ui/ImageLabelingActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -17,7 +18,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
 import com.mikeescom.mlkitexamples.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageLabelingActivity extends AppCompatActivity {
 
@@ -41,6 +51,7 @@ public class ImageLabelingActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mPreviewIv.setImageBitmap(imageBitmap);
+            callFirebaseImageLabelingApi(imageBitmap);
         }
     }
 
@@ -118,5 +129,44 @@ public class ImageLabelingActivity extends AppCompatActivity {
                 .setNegativeButton("Cancel", null)
                 .create()
                 .show();
+    }
+
+    private void callFirebaseImageLabelingApi (Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                .getCloudImageLabeler();
+        labeler.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                        List labelList = new ArrayList();
+                        for (FirebaseVisionImageLabel label: labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            labelList.add(text + "-" + confidence);
+                        }
+                        showLabelsListDialog(labelList);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showLabelsListDialog (List<String> labelList) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Labels found");
+        builder.setItems(labelList.toArray(new String[labelList.size()]), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/mikeescom/mlkitexamples/ui/MLKitExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.mikeescom.mlkitexamples.ui;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class MLKitExamplesApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
